d6c76a9bcb5664a31d8655bb2901cd8a28b71efc
davidoc23 <g00400530@atu.ie>
2023-10-20 12:23:49 +0100

    lab 4 complete

--- src/components/create.js
+++ src/components/create.js
@@ -1,8 +1,48 @@
+import { useState } from "react";
+
 // Defining a functional component named Create
 function Create() {
+   const [title, setTitle] = useState(' ')
+   const [author, setAuthor] = useState(' ')
+   const [cover, setCover] = useState(' ')
+
+   const handleSubmit = (e) => 
+   {
+    e.preventDefault();
+
+    console.log("Title: " + title + " Author: " + author + " Cover: " + cover);
+   }
+
     return (
         <div>
             <h2>Hello from Create Component</h2>
+
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Add Book Title: </label>
+                    <input type="text"
+                    className="form-control"
+                    value={title}
+                    onChange={(e) => { setTitle(e.target.value) }}/>
+                </div>
+                <div className="form-group">
+                    <label>Add Book Author: </label>
+                    <input type="text"
+                    className="form-control"
+                    value={author}
+                    onChange={(e) => { setAuthor(e.target.value) }}/>
+                </div>
+                <div className="form-group">
+                    <label>Add Book Cover: </label>
+                    <input type="text"
+                    className="form-control"
+                    value={cover}
+                    onChange={(e) => { setCover(e.target.value) }}/>
+                </div>
+                <div>
+                    <input type="submit" value="Add Book"></input>
+                </div>
+            </form>
         </div>
     );
 }
